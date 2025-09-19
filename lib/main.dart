50a9807de3149136c55c4026032323e78bbd5a74
MD. FAHIM MEHEMMOD <mdfahimmehemmud@gmail.com>
2025-09-19 11:30:57 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor:  Color.fromARGB(255, 27, 25, 25),
       ),
