9012efdab3574a11ee48faeff743995d89443423
MD. FAHIM MEHEMMOD <mdfahimmehemmud@gmail.com>
2025-09-20 11:59:42 +0600

    commit

--- lib/pages/App Screen/screen_one.dart
+++ lib/pages/App Screen/screen_one.dart
@@ -6,7 +6,66 @@ class screen_one extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 27, 25, 25),
+        actions: [IconButton(onPressed: (){},
+            icon: Icon(Icons.menu,
+              color: Colors.white,))],
+        leading: InkWell(
+          onTap: (){},
+          child: Image.asset("assets/images/img_1.png"),
+        ),
+        title: Text("HI, Susan",
+          style: TextStyle(
+              color: Colors.grey,
+              fontSize: 25),),
+      ),
+      body: ListView(
+        children: [
+          SizedBox(height: 10,),
+          Text("Who do you want to",
+            style: TextStyle(
+                color: Colors.white,
+            fontSize:35),
+          ),
+          Text("chat with today?",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize:35),
+          ),
+          SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 180,width: 180,
+                color: Colors.white
+              ),
+              Container(
+                  height: 180,width: 180,
+                  color: Colors.white
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                  height: 180,width: 180,
+                  color: Colors.white
+              ),
+              Container(
+                  height: 180,width: 180,
+                  color: Colors.white
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          Container(height: 10,width: 10,color: Colors.white,
+          ),
+        ],
+      ),
     );
   }
 }
