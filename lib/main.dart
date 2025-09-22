54ffb8be8377f85895b81c8000b89cf61b34dd52
MD. FAHIM MEHEMMOD <mdfahimmehemmud@gmail.com>
2025-09-22 23:31:18 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:ui_design_application_1/pages/Chat/chat.dart';
 import 'package:ui_design_application_1/pages/Side%20Bar/history.dart';
 import 'package:ui_design_application_1/pages/Side%20Bar/privacy.dart';
 import 'package:ui_design_application_1/pages/Side%20Bar/side_bar.dart';
+import 'package:ui_design_application_1/pages/Splash/NextPage.dart';
 import 'package:ui_design_application_1/pages/Splash/SplashPage.dart';
 import 'package:ui_design_application_1/pages/login%20&%20Authentication/Forget.dart';
 import 'package:ui_design_application_1/pages/login%20&%20Authentication/LoginPage.dart';
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "/": (context) => SplashPage(),
+        "NextPage": (context) => NextPage(),
         "LoginPage": (context) => LoginPage(),
         "Forget": (context) => Forget(),
         "OtpVerify": (context) => OtpVerify(),
